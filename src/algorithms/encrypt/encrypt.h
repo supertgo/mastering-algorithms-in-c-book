72bdf49abac7077301610d3dfb6ef14e02ca3613
supertgo <thiagoroberto2003@gmail.com>
2022-08-10 09:43:52 -0300

    feat(Algorithms): :sparkles: add encrypt header

---
+++ src/algorithms/encrypt/encrypt.h
@@ -0,0 +1,21 @@
+#ifndef ENCRYPT_H
+#define ENCRYPT_H
+
+typedef unsigned long Huge;
+
+typedef struct RsaPriKey_ {
+  Huge d;
+  Huge n;
+} RsaPriKey;
+
+void des_encipher(const unsigned char *plaintext, unsigned char *ciphertext,
+const unsigned char *key);
+
+void des_decipher(const unsigned char *ciphertext, unsigned char *plaintext,
+const unsigned char *key);
+
+void rsa_encipher(Huge plaintext, Huge *ciphertext, RsaPubKey pubkey);
+
+void rsa_decipher(Huge ciphertext, Huge *plaintext, RsaPriKey prikey);
+
+#endif
